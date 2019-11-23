8810be248d763d7da89672d4f2eba34622c9e5a1
wolframowsky <wolframowsky@gmail.com>
2019-11-23 11:42:27 +0100

    Added bg theme and scrpit that plays between scenes

---
+++ Assets/Scripts/BGthemeScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGthemeScript : MonoBehaviour
+{
+
+    private static BGthemeScript instance = null;
+
+    public static BGthemeScript Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+    
+    void Awake()
+    {
+        if(instance !=null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+
+}
---
+++ Assets/Scripts/PauseBgTheme.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        BGthemeScript.Instance.gameObject.GetComponent<AudioSource>().Pause();
+    }
+
+
+}
